cd1dd92648e4c4896095f053dd223fb02cb289b7
Jose Lima <jose.lima@consensote.com.br>
2022-04-30 23:59:56 -0300

    chart state Pernambuco

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,6 +45,14 @@ module.exports = {
       optionalDependencies: ['test/unit/index.js']
     }],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    "comma-dangle": [1, {
+      "objects": "always",
+      "arrays": "ignore",
+      "imports": "ignore",
+      "exports": "ignore",
+      "functions": "ignore"
+    }]
   }
 }
